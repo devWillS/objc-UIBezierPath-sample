8cb8a7cf618104f78d6262c3635fd243ebae8167
Seidi Nakamura <seidi.nakamura@st-ventures.jp>
2019-06-29 17:13:15 +0900

    create class for draw rect

--- objc-UIBezierPath-sample/DrawView.m
+++ objc-UIBezierPath-sample/DrawView.m
@@ -7,6 +7,7 @@
 //
 
 #import "DrawView.h"
+#import "DrawRectWithText.h"
 
 @implementation DrawView
 
@@ -21,33 +22,11 @@
 }
 
 - (void)drawRect:(CGRect)rect {
-    [self draw:CGRectMake(100, 100, 100, 100) withText:@"text1" fontSize:10 backgroundColor:[UIColor colorWithRed:1 green:0.5 blue:0 alpha:1.0]];
+    [DrawRectWithText draw:CGRectMake(100, 100, 100, 100) withText:@"text1" fontSize:10 backgroundColor:[UIColor colorWithRed:1 green:0.5 blue:0 alpha:1.0]];
     
-    [self draw:CGRectMake(50, 150, 100, 100) withText:@"text2" fontSize:23 backgroundColor:[UIColor colorWithRed:0 green:0.5 blue:1 alpha:1.0]];
+    [DrawRectWithText draw:CGRectMake(50, 150, 100, 100) withText:@"text2" fontSize:23 backgroundColor:[UIColor colorWithRed:0 green:0.5 blue:1 alpha:1.0]];
     
-    [self draw:CGRectMake(100, 500, 100, 100) withText:@"text3" fontSize:30 backgroundColor:[UIColor colorWithRed:0.5 green:0.75 blue:0.25 alpha:1.0]];
-}
-
-- (void) draw:(CGRect)rect withText:(NSString *)text fontSize:(CGFloat)fontSize backgroundColor:(UIColor *)color {
-    // 四角形（短形）
-    UIBezierPath *rectangle = [UIBezierPath bezierPathWithRect:rect];
-    // 内側の色
-    [color setFill];
-    // 内側を塗りつぶす
-    [rectangle fill];
-    // 線の色
-    [color setStroke];
-    // 線の太さ
-    [rectangle setLineWidth:2.0f];
-    // 線を塗りつぶす
-    [rectangle stroke];
-    // フォント設定
-    UIFont *font = [UIFont systemFontOfSize:fontSize];
-    
-    NSDictionary *dictionary = [[NSDictionary alloc] initWithObjectsAndKeys: font, NSFontAttributeName,
-                                nil];
-    // テキストを描画
-    [text drawInRect:rect withAttributes:dictionary];
+    [DrawRectWithText draw:CGRectMake(100, 500, 100, 100) withText:@"text3" fontSize:30 backgroundColor:[UIColor colorWithRed:0.5 green:0.75 blue:0.25 alpha:1.0]];
 }
 
 @end
